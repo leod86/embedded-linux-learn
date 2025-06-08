8470f39cd2e82e797ada523eae65cfd0de01986e
leod86 <2018lxd@gmail.com>
2025-06-08 16:55:27 +0800

    day4change

--- day4/cpu.c
+++ day4/cpu.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
+#include <time.h>
 #include "cpu.h"
 
-double get_cpu_usage(void)
+double get_cpu_usage()
 {
     FILE *fp;
     char line[256];
     long user1, nice1, system1, idle1;
     long user2, nice2, system2, idle2;
-    // 第一次读取CPU使用率
-    fp = fopen("/proc/stat", "r"); // 打开文件
+
+    fp = fopen("/proc/stat", "r");
     fgets(line, sizeof(line), fp);
     sscanf(line, "cpu %ld %ld %ld %ld", &user1, &nice1, &system1, &idle1);
-    fclose(fp); // 读完关闭文件
+    fclose(fp);
+
     sleep(1);
 
-    // 第二次读取CPU使用率
     fp = fopen("/proc/stat", "r");
     fgets(line, sizeof(line), fp);
     sscanf(line, "cpu %ld %ld %ld %ld", &user2, &nice2, &system2, &idle2);
@@ -28,17 +28,18 @@ double get_cpu_usage(void)
     long total2 = user2 + nice2 + system2 + idle2;
     long idle_diff = idle2 - idle1;
     long total_diff = total2 - total1;
-    return 100.0 * (1.0 - (doubel)idle_diff / total_diff);
+
+    return 100.0 * (1.0 - (double)idle_diff / total_diff);
 }
 
-void log_cpu_usage(void)
+void log_cpu_usage(double usage)
 {
-    FILE *log_file = fopen("cpu_usage.log", "a");
+    FILE *log_file = fopen("cpu_log.txt", "a");
     if (log_file == NULL)
         return;
 
     time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
+    struct tm *tm_info = localtime(&t);
     char time_str[64];
     strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", tm_info);
 
